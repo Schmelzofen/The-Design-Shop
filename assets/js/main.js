6ebce98be0a02dc67574c283afeb6e54bf43c6f6
Schmelzofen <sertac.oezdemir@gmail.com>
2021-11-06 22:01:30 +0100

    fixed mobile Header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,32 @@
-let overlay = document.getElementById("overlay")
-let main = document.getElementById("main")
-let footer = document.getElementById("footer")
-
+let mobileMenu = document.getElementById("mobileMenu")
+let mobileMenuList = document.getElementById("mobileMenuList")
 let contact = document.getElementById("contact")
 let messageSent = document.getElementById("messageSent")
 
-let email = document.getElementById("email")
-let message = document.getElementById("message")
-let fullName = document.getElementById("fullName")
-
-function openMenu(){
-    overlay.style.height = "100%"
-    overlay.style.opacity = "1"
-    overlay.style.position = "fixed"
-}
-
-function closeMenu(){
-    overlay.style.height = "0%"
-    overlay.style.opacity = "0"
-}
-
-function toContact(){
-    contact.classList.remove("hide")
-    contact.classList.add("show")
-    contact.scrollIntoView()
-    overlay.style.height = "0%"
-    overlay.style.opacity = "0"
-}
-
-function sendMessage(){
-    contact.classList.remove("show")
-    contact.classList.add("hide")
-    messageSent.classList.remove("hide")
-    messageSent.classList.remove("show")
-    console.log(fullName.value)
-    console.log(email.value)
-    console.log(message.value)
-}
-
-function closeWindow(){
-    messageSent.classList.remove("show")
-    messageSent.classList.add("hide")
+function toggleMenu(){
+    mobileMenu.classList.toggle("down")
+    mobileMenuList.classList.toggle("show")
+    mobileMenuList.classList.toggle("transition")
 }
 
 function toContactMobile(){
-    contact.classList.remove("hide")
-    contact.classList.add("show")
-    contact.scrollIntoView()
-    overlay.style.opacity = "0"
+    mobileMenu.classList.toggle("down")
+    mobileMenuList.classList.toggle("show")
+    mobileMenuList.classList.remove("transition")
+    contact.classList.remove("contactShow")
+    messageSent.classList.add("contactShow2")
+}
+
+function toContactDesktop(){
+    contact.classList.remove("contactShow")
+    messageSent.classList.add("contactShow2")
+}
+
+function sendMessage(){
+    contact.classList.toggle("contactShow")
+    messageSent.classList.remove("contactShow2")
+}
+
+function closeWindow(){
+    messageSent.classList.toggle("contactShow2")
 }
